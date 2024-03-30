14260d34486cd292bc404b4b3e8159c8d745436a
mertcetiin <mrtctn506@gmail.com>
2024-03-30 15:56:09 +0300

    footer comp. made

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import { FOOTER_LINKS } from '@/constants';
+import Link from 'next/link';
 
 function Footer() {
     return (
-        <div>Footer</div>
+        <footer className="text-customGray400 font-bold pt-[20px]">
+            <div className="bg-customBlack">
+                <div className="container mx-auto py-6 px-5 flex flex-wrap flex-col sm:flex-row">
+                    <p className="text-customGray400 text-sm text-center sm:text-left">© 2024 —
+                        <Link href="https://www.instagram.com/mertt.cetiin/"
+                            rel="noopener noreferrer"
+                            className="text-customGray400 ml-1"
+                            target="_blank">
+                            @mertcetiin
+                        </Link>
+                    </p>
+
+                    <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start gap-2">
+                        {FOOTER_LINKS.map((link, index) => {
+                            return (
+                                <Link href={link.href} key={index}
+                                    className="text-customGray400 w-5 h-5"
+                                    target='e_blank'>
+                                    {link.iconItem}
+                                </Link>
+                            )
+                        })}
+                    </span>
+
+                </div>
+            </div>
+        </footer>
     )
 }
 

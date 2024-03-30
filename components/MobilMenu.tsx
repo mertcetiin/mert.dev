e9c3fe1a909148db15f04fbf58eb9ba4c745b2c6
mertcetiin <mrtctn506@gmail.com>
2024-03-30 15:51:25 +0300

    header comps. made

--- components/MobilMenu.tsx
+++ components/MobilMenu.tsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import { MOBIL_MENU } from '@/constants';
+import Link from 'next/link';
+
 
 function MobilMenu({ setIsOpenMenu, isOpenMenu }: any) {
     return (
-        <div>MobilMenu</div>
+        <section className="fixed bottom-1 left-2 right-2 sm:hidden">
+
+            <div className='bg-customGray text-customGray600 flex items-center justify-center text-center
+            h-[129px] rounded-3xl gap-x-4'>
+                {MOBIL_MENU.map((menu, index) => {
+                    if (menu.title === 'Resume') {
+                        return (
+                            <Link href={menu.href} key={index}
+                                className='hover:text-customPrimary flex flex-col text-center items-center text-lg'
+                                download
+                                target='e_blank'>
+                                {menu.iconItem}
+                                {menu.title}
+                            </Link>
+                        )
+                    } else if (menu.title === 'close') {
+                        return (
+                            <Link href={menu.href} key={index}
+                                className='text-customWhite flex flex-col text-center items-center text-2xl border rounded-full p-2 bg-customPrimary border-none'
+                                onClick={() => setIsOpenMenu(!isOpenMenu)}>
+                                {menu.iconItem}
+                            </Link>
+                        )
+                    } else {
+                        return (
+                            <Link href={menu.href} key={index} className='hover:text-customPrimary flex flex-col text-center items-center text-lg' >
+                                {menu.iconItem}
+                                {menu.title}
+                            </Link>
+                        )
+                    }
+
+                })}
+
+            </div>
+        </section>
     )
 }
 
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,35 @@
+import { NAVBAR_LINKS } from '@/constants';
 import Link from 'next/link';
 
 
 function Navbar({ containerStyles }: any) {
     return (
         <nav className={`${containerStyles}`}>
-            <div>
-                navbarLink
-            </div>
+            {NAVBAR_LINKS.map((link) => {
+                if (link.key === 'resume') {
+                    return (
+                        <Link
+                            href={link.href}
+                            key={link.key}
+                            className='hover:text-customPrimary'
+                            download
+                            target='e-blank'>
+                            {link.label}
+                        </Link>
+                    )
+                } else {
+                    return (
+                        <Link
+                            href={link.href}
+                            key={link.key}
+                            className='hover:text-customPrimary'
+                        >
+                            {link.label}
+                        </Link>
+                    )
+                }
+
+            })}
         </nav>
     )
 }

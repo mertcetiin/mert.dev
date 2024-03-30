5440bbc7f678001ac1afd1bccb6da9f15f96deb2
mertcetiin <mrtctn506@gmail.com>
2024-03-30 15:42:41 +0300

    Header ve footer comp. created

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <h1>Hello!!!</h1>
+      <h1>Mert Çetin</h1>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,8 +13,25 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        customBlack: '#000000',
+        customGray: '#211F23',
+        customWhite: '#ffff',
+        customBackground: '#F8F9FA',
+        customBorderGray: '#CED4DA',
+        customPurple: '#B292FF',
+        customRed: '#FF3939',
+        customBlue600: '#2563EB',
+        customBlue500: '#3B82F6',
+        customGray600: '#4b5563',
+        customGray400: '#9CA3AF',
+        customTeal500: '#14B8A6',
+        customGray800: '#1F2937',
+        customPrimary: '#539DF3',
+      },
     },
   },
+
   plugins: [],
 };
 export default config;
